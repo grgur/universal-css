dd640f2806abdfc76949c1fe7c2112ff9f8b5a8e
Grgur Grisogono <grgur@grgur.com>
2016-04-21 19:24:42 +0200

    Export criticalCss

--- src/index.js
+++ src/index.js
@@ -2,5 +2,7 @@ export CssProvider, { CssProviderServer, inlineCss } from './CssProvider';
 
 export IsoToolsStyleModules from './IsomorphicToolsStyleModules.js';
 
+export criticalCss from './criticalCss.js';
+
 import UniversalCssLoader from './UniversalCssLoader';
 export default UniversalCssLoader;
