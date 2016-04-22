f3cf7999ab580faa50646e6e157137cdce544c21
Grgur Grisogono <grgur@grgur.com>
2016-04-22 07:16:48 +0200

    Reuse extending isomorphic tools instead requiring again

--- src/IsomorphicToolsStyleModules.js
+++ src/IsomorphicToolsStyleModules.js
@@ -1,13 +1,12 @@
-import WebpackIsomorphicToolsPlugin from 'webpack-isomorphic-tools/plugin';
+const defaultExtensions = [
+  'less',
+  'scss',
+  'css',
+];
 
-export default {
-  extensions: [
-    'less',
-    'scss',
-    'css',
-  ],
-
+export default (WebpackIsomorphicToolsPlugin, extensions=defaultExtensions) => ({
+  extensions,
   filter: WebpackIsomorphicToolsPlugin.style_loader_filter,
   path: WebpackIsomorphicToolsPlugin.style_loader_path_extractor,
   parser: WebpackIsomorphicToolsPlugin.css_modules_loader_parser,
-};
+});
